7246d6a52705c802f6407cc7ccc3a0c4190177a4
BowlBird <bowlbirdcontact@gmail.com>
2024-07-14 11:34:23 -0500

    fixed formatting

--- common/host/.nix-system.nix
+++ common/host/.nix-system.nix
@@ -7,35 +7,35 @@
       TRACE_FLAG="--show-trace"
 
       function nixos_rebuild {
-          local option=$1
-          local verbose=$2
-          local flake="$CONFIG_PATH"
+        local option=$1
+        local verbose=$2
+        local flake="$CONFIG_PATH"
 
-          if [[ -n "$option" ]]; then
-              flake="$flake#$option"
-          fi
+        if [[ -n "$option" ]]; then
+          flake="$flake#$option"
+        fi
 
-          if [[ "$verbose" == "-v" ]]; then
-              $NIXOS_REBUILD --flake "$flake" switch $TRACE_FLAG
-          else
-              $NIXOS_REBUILD --flake "$flake" switch
-          fi
+        if [[ "$verbose" == "-v" ]]; then
+          $NIXOS_REBUILD --flake "$flake" switch $TRACE_FLAG
+        else
+          $NIXOS_REBUILD --flake "$flake" switch
+        fi
       }
 
       function home_manager_rebuild {
-          local option=$1
-          local verbose=$2
-          local flake="$CONFIG_PATH"
+        local option=$1
+        local verbose=$2
+        local flake="$CONFIG_PATH"
 
-          if [[ -n "$option" ]]; then
-              flake="$flake#$option"
-          fi
+        if [[ -n "$option" ]]; then
+          flake="$flake#$option"
+        fi
 
-          if [[ "$verbose" == "-v" ]]; then
-              $HOME_MANAGER --flake "$flake" switch $TRACE_FLAG
-          else
-              $HOME_MANAGER --flake "$flake" switch
-          fi
+        if [[ "$verbose" == "-v" ]]; then
+          $HOME_MANAGER --flake "$flake" switch $TRACE_FLAG
+        else
+          $HOME_MANAGER --flake "$flake" switch
+        fi
       }
 
       function system_clean {
@@ -47,32 +47,32 @@
       }
 
       case $1 in
-          rebuild)
-              case $2 in
-                  host)
-                      nixos_rebuild $3 $4
-                      ;;
-                  home)
-                      home_manager_rebuild $3 $4
-                      ;;
-                  all)
-                      nixos_rebuild $3 $4
-                      home_manager_rebuild $3 $4
-                      ;;
-                  *)
-                      echo "Usage: $0 rebuild {host|home|all} [option] [-v]"
-                      ;;
-              esac
+        rebuild)
+          case $2 in
+            host)
+              nixos_rebuild $3 $4
               ;;
-          clean)
-              system_clean
+            home)
+              home_manager_rebuild $3 $4
               ;;
-          update)
-              system_update
+            all)
+              nixos_rebuild $3 $4
+              home_manager_rebuild $3 $4
               ;;
-          *)
-              echo "Usage: $0 {rebuild {host|home} [option] [-v]|clean}"
+            *)
+              echo "Usage: $0 rebuild {host|home|all} [option] [-v]"
               ;;
+          esac
+          ;;
+        clean)
+          system_clean
+          ;;
+        update)
+          system_update
+          ;;
+        *)
+          echo "Usage: $0 {rebuild {host|home} [option] [-v]|clean}"
+          ;;
       esac
     ''
   )];
