9641f0e5ee365530af14c4a25427f56d90bdcec6
BowlBird <bowlbirdcontact@gmail.com>
2024-07-14 11:05:51 -0500

    system scaffold

--- common/host/.nix-system.nix
+++ common/host/.nix-system.nix
@@ -1,17 +1,68 @@
 { pkgs, ... }: {
   environment.systemPackages = [(
     pkgs.writeScriptBin "system" ''
-      rebuild() {
-        level=$1
-        flake=$2
-        home=$HOME
+      CONFIG_PATH="$HOME/.config/nix"
+      NIXOS_REBUILD="sudo nixos-rebuild"
+      HOME_MANAGER="home-manager"
+      TRACE_FLAG="--show-trace"
 
-        echo $level $flake $home
+      function nixos_rebuild {
+          local option=$1
+          local verbose=$2
+          local flake="$CONFIG_PATH"
 
+          if [[ -n "$option" ]]; then
+              flake="$flake#$option"
+          fi
+
+          if [[ "$verbose" == "-v" ]]; then
+              $NIXOS_REBUILD --flake "$flake" switch $TRACE_FLAG
+          else
+              $NIXOS_REBUILD --flake "$flake" switch
+          fi
       }
-      clean() {
-        echo clean
+
+      function home_manager_rebuild {
+          local option=$1
+          local verbose=$2
+          local flake="$CONFIG_PATH"
+
+          if [[ -n "$option" ]]; then
+              flake="$flake#$option"
+          fi
+
+          if [[ "$verbose" == "-v" ]]; then
+              $HOME_MANAGER --flake "$flake" switch $TRACE_FLAG
+          else
+              $HOME_MANAGER --flake "$flake" switch
+          fi
       }
+
+      function system_clean {
+          nix-collect-garbage
+      }
+
+      case $1 in
+          rebuild)
+              case $2 in
+                  host)
+                      nixos_rebuild $3 $4
+                      ;;
+                  home)
+                      home_manager_rebuild $3 $4
+                      ;;
+                  *)
+                      echo "Usage: $0 rebuild {host|home} [option] [-v]"
+                      ;;
+              esac
+              ;;
+          clean)
+              system_clean
+              ;;
+          *)
+              echo "Usage: $0 {rebuild {host|home} [option] [-v]|clean}"
+              ;;
+      esac
     ''
   )];
 }
