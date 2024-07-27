62ce451bf75825d15309948af757a4584f142728
BowlBird <bowlbirdcontact@gmail.com>
2024-07-27 09:25:42 -0500

    syntax error

--- common/host/niri.nix
+++ common/host/niri.nix
@@ -1,5 +1,4 @@
 { inputs, pkgs, ... }: {
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
-  programs.niri.package = pkgs.niri-unstable;
-  inputs.niri.cache.enable = false;
+  programs.niri.package = pkgs.niri;
 }
