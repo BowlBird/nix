173cad7d94396507c44dda7c8da7eb199385e4b1
BowlBird <bowlbirdcontact@gmail.com>
2024-07-27 13:53:38 -0500

    updating niri config

--- common/host/niri.nix
+++ common/host/niri.nix
@@ -1,7 +1,9 @@
 { pkgs, inputs, ... }: {
   imports = [ inputs.niri.nixosModules.niri ];
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
-  programs.niri.package = pkgs.niri;
-  programs.niri.enable = true;
-  inputs.niri-flake.cache.enable = false;
+  
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri;
+  };
 }
